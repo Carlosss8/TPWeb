1eec02f4a533e55f61bdbe6eb635ab66f858a78f
Carlosss8 <carlosrodriguez8ra@gmail.com>
2024-06-08 20:23:32 -0300

    .

--- TPWeb/Daytona.aspx.cs
+++ TPWeb/Daytona.aspx.cs
@@ -4,20 +4,14 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using System.Xml.Linq;
 
 namespace TPWeb
 {
-    public partial class MasterPage1 : System.Web.UI.MasterPage
+    public partial class Daytona : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
 
         }
-
-        protected void ImageButton1_Click(object sender, ImageClickEventArgs e)
-        {
-        } 
-            
     }
 }
--- TPWeb/Default.aspx.cs
+++ TPWeb/Default.aspx.cs
@@ -5,13 +5,19 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+
 namespace TPWeb
 {
     public partial class Default : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            
+        }
 
+        protected void BtnInformacionDaytona_Click(object sender, EventArgs e)
+        {
+           
         }
     }
 }
